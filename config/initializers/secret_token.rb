3d1428018eedfb3ae90577c788974daf8746f7c1
Taylor Poe <taylorpoe@gmail.com>
2013-10-12 21:25:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mixwod::Application.config.secret_key_base = '4f2e16808f2ecc7e6e12d91505e4aad7f68109e31b66a0f9d0bd0f9ff9de4b6d50e883160f8bbc158acecffef5199589dec5d35daf024dc0c11dd171e8607932'
